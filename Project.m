ad5ed430fc8a439864e38e2860dcac44e733a171
nisarahmad1982 <nisar.ahmad86@gmail.com>
2015-12-18 07:22:02 -0300

    Update Project.m

--- Project.m
+++ Project.m
@@ -38,12 +38,12 @@ max_n =15;
 min_n = 6;
 mpl_gp = getBest(gen_factors,gen_perc,max_n, min_n)
 
-close all
+
 
                             %%%%%%%%%%%%%%%%
                          %%%%  RISK 1 MLP  %%%%
                             %%%%%%%%%%%%%%%%
-%%
+
 mpl_r1 = getBest(R1Input,R1T,max_n, min_n)
  
 
@@ -97,28 +97,34 @@ targetsANFIS1 = xlsread('neural_data.xlsx','N32:N101');
 
 
 
-%% MLP1 %%
+%% MLP1 %% First step
 %%%%%%%%%%
 
 n = randi([min_n,max_n],1,1);
 iterations = 10;
 MLP1Net = generate_mlp(inputsMLP1,targetsMLP1,n);
-close all
 
 %% new trained net
 Perturbed_inputs = [INCPertNewFs(:,1),NewF2,NewF3,NewF4];
 Union1 = [Perturbed_inputs' gen_factors'];
-Union1 = Union1'; % 120x4
+Union1 = Union1';
 %%
-outputsnew = mpl_gp.net(Union1'); % 1x120
+for i=1:120
+outputsnew(i,1:4) = mpl_gp.net(Union1(i,:));
+end
 F5 = [NewF5' C1F1'];
 F5 = F5';
 F6 = [NewF6' C1F2'];
 F6 = F6';
-outputsnew = [outputsnew' F5 F6]; % 120x1 U 120x2
+outputsnew = [outputsnew F5 F6]; % 120x4 U 120x2
 %%
-NewTargetMLP1 = MLP1Net(outputsnew'); % 1x120
-
+%%oldData= [gen_factors C1F1 C1F2];
+%%semifinal = [outputsnew' oldData'];
+%semifinal = semifinal'; % 120x6 
+%%
+for i=1:120
+finalOut(i,1:6) = MLP1Net(outputsnew(i,:));
+end
 
 %% RBF1 %%
 %%%%%%%%%%
@@ -127,9 +133,6 @@ MaxNeurons = 50;
 Spread = 0.5;
 RBF1Net = GenerateRBF(inputsRBF1,targetsRBF1,MaxNeurons,Spread);
 
-%% Unsupervised data
-
-NewTargetRBF1 = RBF1Net(outputsnew'); % 1x120
 %%
 
 %% ANFIS 1 %%
@@ -144,9 +147,8 @@ NumEpochs = 20;
 InputFismat = genfis1(TrainData, NumMfs, MfType);
 [ANFIS1,MseAnfis1] = anfis(TrainData, InputFismat, NumEpochs);
 MinMSEAnfis1 = min(MseAnfis1);
-%%
 
-NewTargetsANFIS1 = evalfis(outputsnew',ANFIS1);
+
 
 %%
                               %%%%%%%%%%%%%%%%%%
@@ -203,6 +205,44 @@ NumEpochs = 20;
 InputFismat = genfis1(TrainData, NumMfs, MfType);
 [ANFIS2,MseAnfis2] = anfis(TrainData, InputFismat, NumEpochs);
 MinMSEAnfis2 = min(MseAnfis2);
+
+%%
+% Calculating Outputs of Classifier2 with Perturbed Inputs 
+R1InputPert = [R1L1Pert' R1L2New R1L3New];
+R2InputPert = [R2L1Pert' R2L2New R2L3New];
+R3InputPert = [R3L1Pert' R3L2New R3L3New];
+
+R1InputPert = [R1InputPert' R1Input'];
+R1InputPert = R1InputPert';
+
+R2InputPert = [R2InputPert' R2Input'];
+R2InputPert = R2InputPert';
+
+R3InputPert = [R3InputPert' R3Input'];
+R3InputPert = R3InputPert';
+
+% Calculating Outputs of Nets with those Perturbed Inputs
+OutMLPR1 = mpl_r1.net(R1InputPert');
+OutMLPR1= OutMLPR1';
+
+OutMLPR2 = mpl_r2.net(R2InputPert');
+OutMLPR2= OutMLPR2';
+
+OutMLPR3 = mpl_r3.net(R3InputPert');
+OutMLPR3= OutMLPR3';
+
+% Combining outputs for Input to C2
+C2InputPert = [OutMLPR1 OutMLPR2 OutMLPR3];
+
+% Calculating Outputs(Targets for C1) from C2
+  NewTargetMLP2 = MLP2Net(C2InputPert');
+  NewTargetMLP2 = NewTargetMLP2';
+  
+  NewTargetRBF2 = RBF2Net(C2InputPert');
+  NewTargetRBF2 = NewTargetRBF2';
+  
+  NewTargetANFIS2 = evalfis(C2InputPert', ANFIS2);
+  
 
 
 
